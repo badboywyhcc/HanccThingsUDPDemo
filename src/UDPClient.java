a3639a67afca0d529286c86ac88cf339bdf86dab
韩帆 <15029561737@163.com>
2019-02-27 13:24:26 +0800

    调整目录结构并修复部分bug

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -1,56 +1,36 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.net.*;
-import java.util.Scanner;
 
 public class UDPClient {
 
     public static void main(String[] args) {
-        try{
-            /*
-             * UDP基本的流程：
-             *  1 创建Socket
-             *  2 准备要发送的数据
-             *  3 准备远端计算机地址信息
-             *  4 打包（设置数据 设置地址）
-             *  5 通过Socket将包发送至远端
-             *  若学要再次发送数据 重复2-5
-             */
-//1
-            DatagramSocket socket=new DatagramSocket();
-//2
-            String message="你好服务端";
-            byte[] data=message.getBytes("UTF-8");
-//3
-            InetAddress address=InetAddress.getByName("localhost");
-            /*
-             * TCP协议的端口与UDO协议的端口是不冲突的
-             */
-            int port=8899;
-//4
-            /*数据所在 数组
-             * 数组长度
-             * 远端地址
-             * 远端端口
-             *
-             */
-            DatagramPacket packet=new  DatagramPacket(data,data.length,address,port);
-//发送
-            socket.send(packet);
-            /*
-             * 接收服务端发送回来的信息
-             */
-            data=new byte[1000];
-            packet=new DatagramPacket(data, data.length);
-//3
-            socket.receive(packet);
-//4
-            message=new String(data,0,packet.getLength(),"Utf-8");
-            System.out.println("服务端说："+message);
-        }catch(Exception e){
+        try {
+            String transData = "你好龟仙人";
+            // 创建发送端对象
+            DatagramSocket client = new DatagramSocket();
+            // 发送
+            DatagramPacket dp = new DatagramPacket(transData.getBytes(), transData.getBytes().length, InetAddress.getLocalHost(), 8899);
+            client.send(dp);
+
+            /*** 接收数据***/
+            byte[] receBuf = new byte[1024];
+            DatagramPacket recePacket = new DatagramPacket(receBuf, receBuf.length);
+            client.receive(recePacket);
+
+            String receStr = new String(recePacket.getData(), 0 , recePacket.getLength());
+            System.out.println("客户端收到回应:" + receStr);
+            System.out.println(recePacket.getPort());
+        } catch (SocketException e) {
             e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+//			// 关闭socket
+//			if(send != null){
+//				send.close();
+//			}
         }
     }
 }
--- src/UDPServer.java
+++ src/UDPServer.java
@@ -1,39 +1,38 @@
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.net.*;
 
 public class UDPServer {
     public static void main(String[] args) {
-        try {
-            // 1.构建DatagramSocket实例，指定本地端口
-            DatagramSocket socket = new DatagramSocket(8899);
-            byte[] buf = new byte[1024];
-            // 2.构建需要收发的DatagramPacket报文--构造 DatagramPacket，用来接收长度为 length 的数据包。
-            DatagramPacket packet = new DatagramPacket(buf, buf.length);
-            while (true) {
-                // 收报文---从此套接字接收数据报包，此方法在接收到数据报前一直阻塞
-                socket.receive(packet);
-                System.out
-                        .println("ip:" + packet.getAddress().getHostAddress());
-                // 接收信息
-                String receive = new String(packet.getData(),packet.getLength());
+        new Thread() {
+            public void run() {
+                try {
+                    // 1.构建DatagramSocket实例，指定本地端口
+                    DatagramSocket server = new DatagramSocket(8899);
+                    byte[] buf = new byte[1024];
+                    // 2.构建需要收发的DatagramPacket报文--构造 DatagramPacket，用来接收长度为 length 的数据包。
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                    while (true) {
+                        // 收报文---从此套接字接收数据报包，此方法在接收到数据报前一直阻塞
+                        server.receive(packet);
+                        // 接收信息
+                        String receive = new String(packet.getData(),0,packet.getLength());
+                        System.out.println("来自IP:"+ packet.getAddress().getHostAddress()+"   PORT:"+packet.getPort()+"   数据:"+receive+"    数据长度:"+packet.getLength());
 
-                System.out.println("接收信息" + packet.getLength());
-                // 发报文
-                String info = receive + "您好！";
-                // 构造数据报包，用来将长度为 length 的包发送到指定主机上的指定端口号。
-                DatagramPacket dp = new DatagramPacket(info.getBytes(),
-                        info.getBytes().length, packet.getAddress(), 8899);
-                // 为此包设置数据缓冲区。将此 DatagramPacket 的偏移量设置为 0，长度设置为 buf 的长度
-                dp.setData(info.getBytes());
-                // 从此套接字发送数据报包
-                socket.send(dp);
+                        // 发报文
+                        String info =  "您好! 已经收到数据了";
+                        // 发送
+                        DatagramPacket dp = new DatagramPacket(info.getBytes(), info.getBytes().length, packet.getAddress(), packet.getPort());
+                        server.send(dp);
+                    }
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-        } catch (SocketException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        }.start();
     }
 }
+
